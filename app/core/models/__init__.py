e6e97af6b3f93fe0b27d1b8839d53c587e32cd3a
m0untain-j3w <adamgunoeff@yandex.ru>
2026-05-19 19:42:13 +0300

    add AccessToken to models init

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "db_helper",
     "Base",
     "User",
+    "AccessToken",
 )
 
+from .access_tokens import AccessToken
 from .db_helper import db_helper
 from .base import Base
 from .users import User

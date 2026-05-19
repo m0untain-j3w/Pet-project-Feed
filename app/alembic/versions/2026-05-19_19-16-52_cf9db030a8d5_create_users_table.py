a2ce1547944f3744c65c101619557093841c8a73
m0untain-j3w <adamgunoeff@yandex.ru>
2026-05-19 19:18:20 +0300

    alembic create users table migration

---
+++ app/alembic/versions/2026-05-19_19-16-52_cf9db030a8d5_create_users_table.py
@@ -0,0 +1,39 @@
+"""create users table
+
+Revision ID: cf9db030a8d5
+Revises:
+Create Date: 2026-05-19 19:16:52.172937
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision: str = "cf9db030a8d5"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(length=320), nullable=False),
+        sa.Column("hashed_password", sa.String(length=1024), nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False),
+        sa.Column("is_verified", sa.Boolean(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(op.f("ix_users_email"), "users", ["email"], unique=True)
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_index(op.f("ix_users_email"), table_name="users")
+    op.drop_table("users")

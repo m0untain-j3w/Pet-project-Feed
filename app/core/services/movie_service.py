236e8029359e48d0b8ce0fe395be5e61fa8eafb2
m0untain-j3w <adamgunoeff@yandex.ru>
2026-05-19 23:17:01 +0300

    create movie service

---
+++ app/core/services/movie_service.py
@@ -0,0 +1,28 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.repositories.movies import MovieRepository
+
+
+class MovieService:
+
+    @staticmethod
+    async def get_movies(
+        session: AsyncSession,
+        limit: int,
+        offset: int,
+    ):
+        return await MovieRepository.get_all(
+            session=session,
+            limit=limit,
+            offset=offset,
+        )
+
+    @staticmethod
+    async def get_movie(
+        session: AsyncSession,
+        movie_id: int,
+    ):
+        return await MovieRepository.get_by_id(
+            session=session,
+            movie_id=movie_id,
+        )

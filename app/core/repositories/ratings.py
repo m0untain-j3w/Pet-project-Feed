b26e6130c45fffcee9b6b40fc82d48096d96b7c3
m0untain-j3w <adamgunoeff@yandex.ru>
2026-05-21 19:37:03 +0300

    create rating repository

---
+++ app/core/repositories/ratings.py
@@ -0,0 +1,53 @@
+from typing import Annotated
+from fastapi import Depends
+from app.core.models import db_helper
+from app.core.models import Rating
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class RatingRepository:
+
+    @staticmethod
+    async def create(
+        session: AsyncSession,
+        user_id: int,
+        movie_id: int,
+        rating: Rating,
+    ) -> Rating:
+        raiting_obj = Rating(
+            user_id=user_id,
+            movie_id=movie_id,
+            rating=rating,
+        )
+        session.add(raiting_obj)
+        await session.commit()
+        await session.refresh(raiting_obj)
+        return raiting_obj
+
+    @staticmethod
+    async def get_user_ratings(
+        session: AsyncSession,
+        user_id: int,
+    ) -> list[Rating]:
+        result = await session.execute(
+            select(Rating)
+            .where(Rating.user_id == user_id)
+            .order_by(Rating.created_at.desc())
+        )
+        return result.scalars().all()
+    
+    @staticmethod
+    async def get_user_movie_rating(
+        session: AsyncSession,
+        user_id: int,
+        movie_id: int,
+    ) -> Rating | None:
+        result = await session.execute(
+            select(Rating)
+            .where(
+                Rating.user_id == user_id,
+                Rating.movie_id == movie_id,
+            )
+        )
+        return result.scalar_one_or_none()

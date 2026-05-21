edb03200551716cdce52562d2a254cfa92063651
m0untain-j3w <adamgunoeff@yandex.ru>
2026-05-21 19:37:13 +0300

    correct movie repository

--- app/core/repositories/movies.py
+++ app/core/repositories/movies.py
@@ -11,30 +11,21 @@ class MovieRepository:
 
     @staticmethod
     async def get_all(
-            session: Annotated[
-                "AsyncSession",
-                Depends(db_helper.session_getter),
-            ],
-            limit: int = 20,
-            offset: int = 0,
+        session: AsyncSession,
+        limit: int = 20,
+        offset: int = 0,
     ):
-        stmt = (
-            select(Movie)
-            .limit(limit)
-            .offset(offset)
+        result = await session.execute(
+            select(Movie).limit(limit).offset(offset),
         )
-
-        result = await session.execute(stmt)
-
         return result.scalars().all()
 
     @staticmethod
     async def get_by_id(
-            session: AsyncSession,
-            movie_id: int,
+        session: AsyncSession,
+        movie_id: int,
     ):
-        stmt = select(Movie).where(Movie.id == movie_id)
-
-        result = await session.execute(stmt)
-
+        result = session.execute(
+            select(Movie).where(Movie.id == movie_id),
+        )
         return result.scalar_one_or_none()

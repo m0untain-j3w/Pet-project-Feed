78c7efa8cf4ae20758057a3748f3fbeb0c6a5306
m0untain-j3w <adamgunoeff@yandex.ru>
2026-05-23 15:29:18 +0300

    add from_attributes in RatingRead schema

--- app/core/schemas/rating.py
+++ app/core/schemas/rating.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class RatingCreate(BaseModel):
@@ -9,6 +9,8 @@ class RatingCreate(BaseModel):
 
 
 class RatingRead(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
     id: int
     user_id: int
     movie_id: int
